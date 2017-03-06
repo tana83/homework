566f93362a30ce73c39ee2cccaffeb5fc8943320
natalia yarlykova <yrlykv@yahoo.com>
2017-03-06 15:06:06 -0500

    initial edit

--- javascript_basics/js_basics.js
+++ javascript_basics/js_basics.js
@@ -5,36 +5,3 @@ console.log(a+b)
 var c = "Cats don't like dogs";
 var d = "Dogs don't like cats";
 console.log(c+d)
-
-var dogs = ["Pitbull", "Siberean Husky", "Pug", "Beagle"];
-var cats = ["Persian cat", "Siamse cat", "Ragdoll", "Bengal cat"];
-var animals = [dogs, cats];
-var animals = [["Pitbull", "Siberean Husky", "Pug", "Beagle"], ["Persian cat", "Siamse cat", "Ragdoll", "Bengal cat"]]
-console.log(animals[0][2])
-console.log(animals[1][3])
-
-k = 99
-if (k<100){
- alert("the variable is less than a hundred")
-} else {
-  console.log("the variable is greater than a hundred");
-}
-
-function alertName(Name){
-  return alert(Name);
-}
-alertName ("Mike")
-alertName ("David")
-
-function enter(door){
-  if(door == 1){
-    alert("trip to Vegas");
-  } else if(door == 2){
-    alert("stay in a 5-star hotel");
-  } else{
-    alert("start all over again");
-  }
-}
-enter(1)
-enter(2)
-enter(3)
